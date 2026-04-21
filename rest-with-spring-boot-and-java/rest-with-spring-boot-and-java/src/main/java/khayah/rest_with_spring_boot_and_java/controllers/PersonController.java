36a5c3919ae88890b8a5d7e30604fe978ac18703
MikhayahIV <sasha.vessa@gmail.com>
2026-04-21 16:22:29 -0300

    feat: implement simple Person methods with mock data

---
+++ rest-with-spring-boot-and-java/rest-with-spring-boot-and-java/src/main/java/khayah/rest_with_spring_boot_and_java/controllers/PersonController.java
@@ -0,0 +1,67 @@
+package khayah.rest_with_spring_boot_and_java.controllers;
+
+import khayah.rest_with_spring_boot_and_java.model.Person;
+import khayah.rest_with_spring_boot_and_java.services.PersonServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.*;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+    @Autowired
+    private PersonServices service;
+
+    @RequestMapping(
+            value = "/{id}",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Person findById(@PathVariable("id") String id){
+       return service.findById(id);
+    }
+
+    @RequestMapping(
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public List<Person> findAll(){
+        return service.findAll();
+    }
+
+
+    @RequestMapping(
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Person create(@RequestBody Person person){
+        return service.create(person);
+    }
+
+    @RequestMapping(
+            method = RequestMethod.PUT,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Person update(@RequestBody Person person){
+        return service.create(person);
+    }
+
+    @RequestMapping(value = "/{id}",
+            method = RequestMethod.DELETE
+    )
+    public void delete(@PathVariable("id") String id){
+        service.delete(id);
+    }
+
+
+
+
+
+}
--- rest-with-spring-boot-and-java/rest-with-spring-boot-and-java/src/main/java/khayah/rest_with_spring_boot_and_java/model/Greeting.java
+++
@@ -1,6 +0,0 @@
-package khayah.rest_with_spring_boot_and_java.model;
-
-public record Greeting(
-        Long id,
-        String content
-){}
